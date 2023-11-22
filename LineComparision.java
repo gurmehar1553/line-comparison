6689f533a5192ec1e03be2df5ab45c96131563b5
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-22 18:58:15 +0530

    Initial commit

---
+++ LineComparision.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class LineComparison{
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+		int x1,y1,x2,y2;
+		x1 = sc.nextInt();
+		y1 = sc.nextInt();
+		x2 = sc.nextInt();
+		y2 = sc.nextInt();
+	}
+}
