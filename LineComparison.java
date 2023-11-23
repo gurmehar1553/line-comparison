3edb6dc0a9a49b089cb82710dc372c3137fc41dc
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-23 16:40:18 +0530

    Refracted code

--- LineComparison.java
+++ LineComparison.java
@@ -1,6 +1,13 @@
 import java.util.*;
 
 public class LineComparison{
+
+	public static double findLength(int x1,int y1,int x2,int y2){
+		double length = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		System.out.println("Length of line : "+length);
+		return length;
+	}
+	
 	public static void main(String[] args){
 
 		Scanner sc = new Scanner(System.in);
@@ -10,14 +17,13 @@ public class LineComparison{
 		x2 = sc.nextInt();
 		y2 = sc.nextInt();
 
-		double length1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-		System.out.println("Length of line : "+length1);
+		double length1 = findLength(x1,y1,x2,y2);
 
 		int a1=sc.nextInt();
 		int b1=sc.nextInt();
 		int a2=sc.nextInt();
 		int b2=sc.nextInt();
-		double length2 = Math.sqrt((a2-a1)*(a2-a1) + (b2-b1)*(b2-b1));
+		double length2 = findLength(a1,b1,a2,b2);
 		
 		if(length1 == length2){
 			System.out.println("Two lines are equal");
